e909c64aefdc4827f2484628c101b34fd0537101
Kate Hill <katelynjayne20@gmail.com>
2024-11-22 20:48:53 -0700

    actually fixing graph problem

--- src/stats/stats.jsx
+++ src/stats/stats.jsx
@@ -11,45 +11,37 @@ function paceStringConversion(paceStr) {
     return (parseInt(pieces[0]) + (seconds/60)).toString();
 }
 
-function graph() {
-    const [graphContent, setGraphContent] = React.useState(<p>Loading...</p>);
-
-    React.useEffect(() => {
-        async function generateGraph() {
-            const allDataJson = await fetch('api/runs');
-            const allData = await allDataJson.json();
-            if (allData.length <= 2) {
-                setGraphContent(<p>Add more runs to see your pace graph!</p>);
-            } else {
-                let paces = ""
-                let dates = "|"
-                for (let run of allData.slice(0,10).reverse()) {
-                    paces += paceStringConversion(run.pace) + ","
-                    let datePieces = run.date.split('-')
-                    dates += datePieces[1]+'/'+datePieces[2] + "|"
-                }
-                paces = paces.substring(0, paces.length-1)
-                const [success, setSuccess] = React.useState(false);
-                const graphLink = `https://image-charts.com/chart?cht=ls&chd=t:${paces}&chs=800x500&chco=606445&chls=5&chxt=x&chxl=0:${dates}&chg=1&chf=bg,s,F3C79E&chtt=Average+Pace+of+Recent+Runs&chts=606445,30`
-                fetch(graphLink)
-                    .then(() => setSuccess(true))
-                    .catch(() => setSuccess(false));
-                if (success) {
-                    setGraphContent(<img src={graphLink} width="75%"></img>);
-                } else {
-                    setGraphContent(<p>Sorry, we had a problem rendering your graph. :(</p>);
-                }
-            }
-        }
-        generateGraph();
-    }, []);
-
-    return graphContent;
-}
-
 export function Stats(props) {
     const location = useLocation();
     const { state } = location;
+
+        const [graphContent, setGraphContent] = React.useState(<p>Loading...</p>);
+
+
+        React.useEffect(() => {
+            async function generateGraph() {
+                const allDataJson = await fetch('api/runs');
+                const allData = await allDataJson.json();
+                if (allData.length <= 2) {
+                    setGraphContent(<p>Add more runs to see your pace graph!</p>);
+                } else {
+                    let paces = ""
+                    let dates = "|"
+                    for (let run of allData.slice(0,10).reverse()) {
+                        paces += paceStringConversion(run.pace) + ","
+                        let datePieces = run.date.split('-')
+                        dates += datePieces[1]+'/'+datePieces[2] + "|"
+                    }
+                    paces = paces.substring(0, paces.length-1)
+                    const graphLink = `https://image-charts.com/chart?cht=ls&chd=t:${paces}&chs=800x500&chco=606445&chls=5&chxt=x&chxl=0:${dates}&chg=1&chf=bg,s,F3C79E&chtt=Average+Pace+of+Recent+Runs&chts=606445,30`
+                    fetch(graphLink)
+                        .then(() => setGraphContent(<img src={graphLink} width="75%"></img>))
+                        .catch(() => setGraphContent(<p>Sorry, we had a problem rendering your graph. :(</p>));
+                }
+            }
+            generateGraph();
+        }, []);
+    
 
     return (
         <main>
@@ -73,7 +65,7 @@ export function Stats(props) {
                     </tr>
                 </tbody>
             </table>
-            {graph()}
+            {graphContent}
             <Comments userName = {props.userName}/>
         </main>
     );
