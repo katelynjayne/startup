cc4ce411e1d3394eb82e65f83beb2a5c64f8bc01
Kate Hill <katelynjayne20@gmail.com>
2024-12-04 21:27:47 -0700

    code cleanup and remove unnecessary parts

--- service/ws.js
+++ service/ws.js
@@ -1,58 +1,51 @@
 const { WebSocketServer } = require('ws');
-const uuid = require('uuid');
 
 function ws(httpServer) {
-  // Create a websocket object
-  const wss = new WebSocketServer({ noServer: true });
+    const wss = new WebSocketServer({ noServer: true });
 
-  // Handle the protocol upgrade from HTTP to WebSocket
-  httpServer.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request);
-    });
-  });
-
-  // Keep track of all the connections so we can forward messages
-  let connections = [];
-
-  wss.on('connection', (ws) => {
-    const connection = { id: uuid.v4(), alive: true, ws: ws };
-    connections.push(connection);
-
-    // Forward messages to everyone INCLUDING the sender
-    ws.on('message', function message(data) {
-      connections.forEach((c) => {
-          c.ws.send(data);
-      });
+    httpServer.on('upgrade', (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
     });
 
-    // Remove the closed connection so we don't try to forward anymore
-    ws.on('close', () => {
-      const pos = connections.findIndex((o, i) => o.id === connection.id);
+    let connections = [];
 
-      if (pos >= 0) {
-        connections.splice(pos, 1);
-      }
+    wss.on('connection', (ws) => {
+        const connection = {alive: true, ws: ws };
+        connections.push(connection);
+
+
+        ws.on('message', function message(data) {
+            connections.forEach((c) => {
+                c.ws.send(data);
+            });
+        });
+
+        ws.on('close', () => {
+            const pos = connections.findIndex((o, i) => o.id === connection.id);
+
+            if (pos >= 0) {
+                connections.splice(pos, 1);
+            }
+        });
+
+        ws.on('pong', () => {
+            connection.alive = true;
+        });
     });
 
-    // Respond to pong messages by marking the connection alive
-    ws.on('pong', () => {
-      connection.alive = true;
-    });
-  });
 
-  // Keep active connections alive
-  setInterval(() => {
-    connections.forEach((c) => {
-      // Kill any connection that didn't respond to the ping last time
-      if (!c.alive) {
-        c.ws.terminate();
-      } else {
-        c.alive = false;
-        c.ws.ping();
-      }
-    });
-  }, 10000);
+    setInterval(() => {
+        connections.forEach((c) => {
+            if (!c.alive) {
+                c.ws.terminate();
+            } else {
+                c.alive = false;
+                c.ws.ping();
+            }
+        });
+    }, 10000);
 }
 
 module.exports = { ws };
